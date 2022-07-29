784bf8a7be45e75bfc0ed0e9afb43f6c46ce6059
Inan404 <76995150+Inan404@users.noreply.github.com>
2022-07-29 11:47:51 +0600

    Time: 4 ms (24.21%), Space: 5.8 MB (88.71%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -0,0 +1,18 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int l = 0 , r = n , m ;
+        
+        while(l <= r){
+            m  = l + (r-l)/2 ; 
+            
+            if(isBadVersion(m) == false) l  = m +1;
+            else r = m - 1 ;
+            
+        }
+        return l;
+    }
+};
